3352a015639bc7f44dda0e241b645865ae5fdd4b
Kate Hudson <k88hudson@gmail.com>
2013-11-05 08:37:44 -0500

    Various updates

--- app/index.js
+++ app/index.js
@@ -3,12 +3,13 @@ var util = require('util');
 var path = require('path');
 var yeoman = require('yeoman-generator');
 
-
 var ExpGenerator = module.exports = function ExpGenerator(args, options, config) {
   yeoman.generators.Base.apply(this, arguments);
 
   this.on('end', function () {
-    this.installDependencies({ skipInstall: options['skip-install'] });
+    this.installDependencies({
+      skipInstall: options['skip-install']
+    });
   });
 
   this.pkg = JSON.parse(this.readFileAsString(path.join(__dirname, '../package.json')));
@@ -22,46 +23,30 @@ ExpGenerator.prototype.askFor = function askFor() {
   // have Yeoman greet the user.
   console.log(this.yeoman);
 
-  var prompts = [
-    {
-      type: 'list',
-      name: 'cssEngine',
-      message: 'Which CSS preprocessor do you like?',
-      choices: [
-        "less",
-        "stylus"
-      ],
-      default: "less"
-    },
-    {
-      name: 'expName',
-      message: 'What would you like to name your experiment?',
-      default: 'my-exp'
-    },
-    {
-      type: 'list',
-      name: 'templateEngine',
-      message: 'Which templating engine?',
-      choices: [
-        "jade",
-        "nunjucks"
-      ],
-      default: "nunjucks"
-    },
-    {
-      type: 'confirm',
-      name: 'useFontAwesome',
-      message: 'Would you like to use Font Awesome?',
-      default: true
-    }
-  ];
+  var prompts = [{
+    name: 'expName',
+    message: 'What would you like to name your experiment?',
+    default: 'my-exp'
+  }, {
+    type: 'list',
+    name: 'templateEngine',
+    message: 'Which templating engine do you prefer?',
+    choices: [
+      "jade",
+      "nunjucks"
+    ],
+    default: "nunjucks"
+  }, {
+    type: 'confirm',
+    name: 'useFontAwesome',
+    message: 'Would you like to use Font Awesome?',
+    default: true
+  }];
 
   this.prompt(prompts, function (props) {
     this.expName = props.expName;
     this.useNunjucks = props.templateEngine === 'nunjucks';
     this.useJade = props.templateEngine === 'jade';
-    this.useLess = props.cssEngine === 'less';
-    this.useStylus = props.cssEngine === 'stylus';
     this.useFontAwesome = props.useFontAwesome;
 
     cb();
@@ -72,26 +57,41 @@ ExpGenerator.prototype.projectFiles = function projectFiles() {
   this.copy('jshintrc', '.jshintrc');
   this.copy('gitignore', '.gitignore');
   this.copy('Procfile', 'Procfile');
+  this.template('_README.md', 'README.md');
 };
 
 ExpGenerator.prototype.configFiles = function configFiles() {
-  this.copy('_package.json', 'package.json');
-  this.copy('_bower.json', 'bower.json');
-  this.copy('env', '.env');
+  this.template('_package.json', 'package.json');
+  this.template('_bower.json', 'bower.json');
+  this.template('Gruntfile.js', 'Gruntfile.js');
+  this.template('env', '.env');
+  this.copy('env', '.env-dist');
 };
 
 ExpGenerator.prototype.app = function app() {
-  this.mkdir('views');
   this.mkdir('routes');
 
   this.template('_app.js', 'app.js');
   this.template('routes/index.js', 'routes/index.js');
+
+  // For precompiling
+  if (this.useNunjucks) {
+    this.mkdir('lib');
+    this.mkdir('lib/grunt');
+    this.copy('grunt-nunjucks.js', 'lib/grunt/grunt-nunjucks.js');
+  }
 };
 
 ExpGenerator.prototype.frontEnd = function frontEnd() {
+  this.mkdir('src');
+  this.mkdir('dist');
   this.mkdir('public');
-  this.template('public/main.js', 'public/main.js');
-  if (this.useLess) {
-    this.template('public/main.less', 'public/main.less');
+  if (this.useNunjucks) {
+    this.template('src/index.html', 'src/index.html');
   }
+  if (this.useJade) {
+    this.template('src/index.jade', 'src/index.jade');
+  }
+  this.template('src/main.js', 'src/main.js');
+  this.template('src/main.less', 'src/main.less');
 };
--- app/templates/_app.js
+++ app/templates/_app.js
@@ -1,12 +1,7 @@
 var express = require('express');
-var Habitat = require('habitat');<% if (useLess) { %>
-var lessMiddleware = require('less-middleware');<% } %><% if (useStylus) { %>
-var stylus = require('stylus');
-var nib = require('nib');<% } %><% if (useNunjucks) { %>
-var nunjucks = require('nunjucks');<% } %><% if (useJade) { %>
-var jade = require('jade');<% } %>
-var path = require('path');
-var routes = require('./routes');
+var Habitat = require('habitat');<% if (useNunjucks) { %>
+var nunjucks = require('nunjucks');<% } %>
+var routes = require('./routes')();
 
 Habitat.load();
 
@@ -14,49 +9,24 @@ var app = express();
 var env = new Habitat();
 var optimize = env.get('OPTIMIZE');
 <% if (useNunjucks) { %>
-var nunjucksEnv = new nunjucks.Environment(new nunjucks.FileSystemLoader(path.join(__dirname, 'views')), {
+var nunjucksEnv = new nunjucks.Environment(new nunjucks.FileSystemLoader(__dirname + '/src'), {
   autoescape: true
 });<% } %>
-var tmpDir = path.join(require('os').tmpDir(), '<%= _.slugify(expName) %>');
+var cacheSettings = optimize ? { maxAge: '31556952000' } : undefined; // one year;
 
 app.locals({
   OPTIMIZE: env.get('OPTIMIZE')
-});<% if (useNunjucks) { %>
+});
+<% if (useNunjucks) { %>
 nunjucksEnv.express(app);<% } %>
-
-app.use(express.logger());
+<% if (useJade) { %>
+app.set('view engine', 'jade');
+app.set('views', __dirname + '/src');<% } %>
+app.use(express.logger('dev'));
 app.use(express.compress());
-<% if (useLess) { %>
-app.use(lessMiddleware({
-  once: optimize,
-  dest: tmpDir,
-  src: __dirname + '/public',
-  paths: __dirname,
-  compress: optimize,
-  yuicompress: optimize,
-  optimization: optimize ? 0 : 2
-}));<% } %><% if (useStylus) { %>
-function stylusCompile(str, path) {
-  return stylus(str)
-    .set('filename', path)
-    .set('compress', optimize)
-    .use(nib());
-}
-app.use(stylus.middleware({
-  src: path.join(__dirname, '/public'),
-  compile: stylusCompile
-}));
-<% } %>
-var cacheSettings = optimize ? { maxAge: '31556952000' } : undefined; // one year;
-
-app.use(express.static(__dirname + '/public', {
-  maxAge: '31556952000' // one year
-}));
-
-app.use(express.static(tmpDir, cacheSettings));
+app.use(express.static(__dirname + '/dist', cacheSettings));
 app.use(express.static(__dirname + '/public', cacheSettings));
 app.use('/bower_components', express.static(__dirname + '/bower_components', cacheSettings));
-app.use('/views', express.static(__dirname + '/views', cacheSettings));
 
 app.use(app.router);
 
